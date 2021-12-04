cade2c41446319012b0111607a5b8e23f8b39d5c
0xluminous <0xluminous@protonmail.com>
2021-12-03 17:13:33 -0800

    sorting by priority

--- faqs/what-is-saito.md
+++ faqs/what-is-saito.md
@@ -1,6 +1,7 @@
 ---
 title: What is Saito?
 source: https://org.saito.tech/faq/
+priority: 10
 ---
 
 Saito is a layer-one blockchain. The network is noteworthy for being more secure than Bitcoin while making payments not just to miners and stakers but also the nodes in the network that offer data-services to users in the network.
--- src/faqs.js
+++ src/faqs.js
@@ -5,8 +5,15 @@ const path = require("path");
 
 const faqsDirectory = path.join(process.cwd(), "faqs");
 
-export function getAll() {
-  return parseAll(glob.sync(path.join(faqsDirectory, "*.md")));
+export function getAll(sorted=true) {
+  const faqs = parseAll(glob.sync(path.join(faqsDirectory, "*.md")));
+  if (!sorted) { return faqs }
+  faqs.sort((a, b) => {
+    if (a.data.priority > b.data.priority) { return -1 }
+    if (a.data.priority < b.data.priority) { return 1 }
+    return 0;
+  });
+  return faqs;
 }
 
 export function parseAll(filenames) {
@@ -25,6 +32,7 @@ function slugifyFilepath(filepath) {
 export function parse(filename) {
   const obj = matter.read(filename);
   obj.slug = slugifyFilepath(obj.path);
+  obj.data.priority = Number(obj.data.priority || 0);
   delete obj.orig;
   return obj;
 }
